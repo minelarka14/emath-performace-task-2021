13e279d60ec4af619ef0fe02e76d8b7fd068d53b
Muhammad <muhammadosaid06@gmail.com>
2021-03-31 18:22:12 +0800

    Created output file system

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import pandas as pd
 
 df = pd.read_csv('./data/a.csv', index_col='Country/Region')
-print(df.columns) # first at 1/22/20 ; last at 3/29/21
+# print(df.columns)  first at 1/22/20 ; last at 3/29/21 start at apr 4, at start=87
 a = []
-for i, b in enumerate(df.loc['US'][(3 + 61):int(7*30.5)]):
-    if not i % 5:
-        print(b, end=" ")
+def printData(country, start=86, end=87, sep=5):
+    iout = ''
+    bout = ''
+    for i, b in enumerate(df.loc[country][start:-end]):
+        if not i % sep:
+            iout += f' {i}'
+    for i, b in enumerate(df.loc[country][start:-end]):
+        print(i, b)
+        if not i % sep:
+            bout += f' {b}'
+    with open('./out/out.txt', 'a') as f:
+        f.write(f'{country}\n{iout}\n{bout}\n')
+    print(iout)
+
+printData('Singapore', sep=1)
+printData('US', sep=1)
+printData('Taiwan*', sep=1)
